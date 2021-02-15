a869eeea749c04c2049f4ceb528a7e445314688f
Andreas Altendorfer <andreas@altendorfer.at>
2021-02-15 21:55:34 +0100

    Shift Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Seely.MixProject do
     [
       app: :seely,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: compiler_paths(Mix.env()),
